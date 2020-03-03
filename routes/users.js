defc369e668b17021034b8c9c85521f3835851b0
Cody.Driver <dricodp@dunwoody.edu>
2020-03-03 12:42:51 -0600

    Finishing project

--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,12 @@ var fs = require('fs');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  let userData = fs.readFileSync('./users.json');
+  var siteUsers = JSON.parse(userData);
+
+  //Assigning the parsed array of objects read-in from users.json to a variable called createdUsers
+  var createdUsers = siteUsers;
+  res.render('users', {createdUsers});
 });
 
 module.exports = router;
-let userData = fs.readFileSync('./users.json');
-var siteUsers = JSON.parse(userData);
-
-//Assigning the parsed array of objects read-in from users.json to a variable called createdUsers
-var createdUsers = siteUsers;
-res.render('users', {createdUsers});
